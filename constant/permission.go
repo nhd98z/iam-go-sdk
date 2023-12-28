03ed64bb24e9943c79502626a44c80d23bedd8cf
anhnv <anhvietnguyen.nva@gmail.com>
2023-12-28 12:44:05 +0700

    fix: fix IAMPermissionCreateSubjectRelationTupleMultipleObjectsDefaultURL

--- constant/permission.go
+++ constant/permission.go
@@ -2,7 +2,7 @@ package constant
 
 const (
 	IAMPermissionCreateSubjectRelationTupleDefaultURL                = "http://iam-permission-admin-api.iam.svc:8080/admin/api/v1/subject-relation-tuples"
-	IAMPermissionCreateSubjectRelationTupleMultipleObjectsDefaultURL = "http://iam-permission-admin-api.iam.svc:8080/admin/api/v1/subject-relation-tuples/multilple-objects"
+	IAMPermissionCreateSubjectRelationTupleMultipleObjectsDefaultURL = "http://iam-permission-admin-api.iam.svc:8080/admin/api/v1/subject-relation-tuples/multiple-objects"
 	IAMPermissionCheckPermissionsDefaultURL                          = "http://iam-permission-public-api.iam.svc:8080/api/v1/permissions/check"
 
 	IAMPermissionRelationViewer   = "viewer"
